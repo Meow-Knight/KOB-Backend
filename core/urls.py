fe6cbeae06cfa25f5d78268e87a3bc6df0757ac0
huyviet2582000 <huyviet2582000@gmail.com>
2021-12-04 00:34:19 +0700

    [FEAT] Create Account model and SignInWithGG API

--- core/urls.py
+++ core/urls.py
@@ -36,6 +36,8 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+from api_account.views import GoogleLogin
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
@@ -44,4 +46,7 @@ urlpatterns = [
     url(r"api/v1/account/", include('api_account.urls')),
     url(r"api/v1/admin/", include('api_admin.urls')),
     url(r"api/v1/beer/", include('api_beer.urls')),
+    url(r'auth/', include('dj_rest_auth.urls')),
+    url(r'^accounts/', include('allauth.urls'), name='socialaccount_signup'),
+    url(r'social-login/google/', GoogleLogin.as_view(), name='google_login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
