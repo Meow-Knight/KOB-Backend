7621e1a45bcab2dd9b58c896cfe1039c1cfea994
bangpham <bangpham2501@gmail.com>
2021-12-26 18:32:38 +0700

    [FEAT] Create beer shipment model

--- api_beer/models/__init__.py
+++ api_beer/models/__init__.py
@@ -3,4 +3,5 @@ from .Nation import Nation
 from .BeerUnit import BeerUnit
 from .Beer import Beer
 from .BeerPhoto import BeerPhoto
+from .BeerShipment import BeerShipment
 from .BeerDiscount import BeerDiscount
