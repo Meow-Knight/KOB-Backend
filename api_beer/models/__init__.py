fa9ba736d1d27276dd9d5e351284371a4973e017
huyviet2582000 <huyviet2582000@gmail.com>
2021-12-28 00:17:41 +0700

    [FIX] Change discount model

--- api_beer/models/__init__.py
+++ api_beer/models/__init__.py
@@ -3,5 +3,6 @@ from .Nation import Nation
 from .BeerUnit import BeerUnit
 from .Beer import Beer
 from .BeerPhoto import BeerPhoto
-from .BeerShipment import BeerShipment
+from .Discount import Discount
 from .BeerDiscount import BeerDiscount
+from .BeerShipment import BeerShipment
