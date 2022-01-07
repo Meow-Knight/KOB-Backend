c2bf18be127aab9dae9d07697104c3450700c496
huyviet2582000 <huyviet2582000@gmail.com>
2022-01-07 22:41:28 +0700

    [FEAT] Create CRUD Cart APIs (user)

--- api_beer/serializers/__init__.py
+++ api_beer/serializers/__init__.py
@@ -3,6 +3,6 @@ from .BeerUnit import BeerUnitSerializer
 from .Nation import NationSerializer
 from .Beer import BeerSerializer, ListBeerSerializer, RetrieveBeerSerializer, ItemBeerSerializer
 from .BeerShipment import BeerShipmentSerializer, ListBeerShipmentSerializer
-
+from .Cart import CUCartSerializer, BeerDetailCartSerializer
 from .BeerDetail import BeerDetailSerializer, ItemBeerSerializer
 from .Discount import DiscountWithItemBeerSerializer
