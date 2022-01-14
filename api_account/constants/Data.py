45d01fa2eb6ac06df913dd354d8fcf8278f848a3
bangpham <bangpham2501@gmail.com>
2022-01-14 13:08:56 +0700

    [FEAT] Order Checkout user

---
+++ api_account/constants/Data.py
@@ -0,0 +1,178 @@
+class AccountData:
+    accounts = [
+        {
+            "id": 2,
+            "email": "huyviet2582000@gmail.com",
+            "first_name": "Huy",
+            "last_name": "Viet"
+        },
+        {
+            "id": 3,
+            "email": "gonhungho123@gmail.com",
+            "first_name": "Kiet",
+            "last_name": "Tuan"
+        },
+        {
+            "id": 4,
+            "email": "ductai26998@gmail.com",
+            "first_name": "Tai",
+            "last_name": "Duc"
+        },
+        {
+            "id": 5,
+            "email": "kimchilethi411@gmail.com",
+            "first_name": "Chi",
+            "last_name": "Kim"
+        },
+        {
+            "id": 6,
+            "email": "bangpham2501@gmail.com",
+            "first_name": "Bang",
+            "last_name": "Van"
+        },
+        {
+            "id": 7,
+            "email": "longvavet1325@gmail.com",
+            "first_name": "Long",
+            "last_name": "Ngoc"
+        },
+        {
+            "id": 8,
+            "email": "Kimhoangle.23022000@gmail.com",
+            "first_name": "Hoang",
+            "last_name": "Kim"
+
+        },
+        {
+            "id": 9,
+            "email": "nguyencongvinh1412@gmail.com",
+            "first_name": "Vinh",
+            "last_name": "Cong"
+        },
+        {
+            "id": 10,
+            "email": "quangnhat358@gmail.com",
+            "first_name": "Nhật",
+            "last_name": "Quang"
+        },
+        {
+            "id": 11,
+            "email": "quocbao080220@gmail.com",
+            "first_name": "Bảo",
+            "last_name": "Quốc"
+        },
+        {
+            "id": 12,
+            "email": "nguyen2924092000@gmail.com",
+            "first_name": "Vinh",
+            "last_name": "Cong"
+        },
+        {
+            "id": 13,
+            "email": "nguyenvantien3042k@gmail.com",
+            "first_name": "Tiên",
+            "last_name": "Văn"
+        },
+        {
+            "id": 14,
+            "email": "duongthanhdung612@gmail.com",
+            "first_name": "Dũng",
+            "last_name": "Thanh"
+        },
+        {
+            "id": 15,
+            "email": "huyentrang27092000@gmail.com",
+            "first_name": "Trang",
+            "last_name": "Huyền"
+        },
+
+    ]
+
+
+class UserData:
+    users = [
+        {
+            "id": 2,
+            "address": "Nhị Dinh 3, Điện Phước, Điện Bàn, Quảng Nam",
+            "phone": "0909079675",
+            "age": 21
+        },
+        {
+            "id": 3,
+            "address": "161 Nguyễn Phong Sắc, Hưng Dũng, Thành phố Vinh, Nghệ An",
+            "phone": "079475834",
+            "age": 19
+        },
+        {
+            "id": 4,
+            "address": "15 Hùng Vương, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0909079675",
+            "age": 21
+        },
+        {
+            "id": 5,
+            "address": "28 Ngô Xuân Thu, Hoà Hiệp Bắc, Liên Chiểu, Đà Nẵng",
+            "phone": "098567812",
+            "age": 20
+        },
+        {
+            "id": 6,
+            "address": "23 Quang Trung, Thành phố Vinh, Nghệ An",
+            "phone": "097856764",
+            "age": 20
+        },
+        {
+            "id": 7,
+            "address": "96 Quách Thị Trang, Hoà Xuân, Cẩm Lệ, Đà Nẵng",
+            "phone": "0974754560",
+            "age": 21
+        },
+        {
+            "id": 8,
+            "address": "252 Huỳnh Ngọc Huệ, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0974754560",
+            "age": 21
+        },
+        {
+            "id": 9,
+            "address": "15 Hùng Vương, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0327949281",
+            "age": 21
+        },
+        {
+            "id": 10,
+            "address": "15 Hùng Vương, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0327949281",
+            "age": 21
+        },
+        {
+            "id": 11,
+            "address": "15 Hùng Vương, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0327949281",
+            "age": 21
+        },
+        {
+            "id": 12,
+            "address": "15 Hùng Vương, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0327949281",
+            "age": 21
+        },
+        {
+            "id": 13,
+            "address": "15 Hùng Vương, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0327949281",
+            "age": 21
+        },
+        {
+            "id": 14,
+            "address": "15 Hùng Vương, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0327949281",
+            "age": 21
+        },
+        {
+            "id": 15,
+            "address": "15 Hùng Vương, TT. ái Nghĩa, Đại Lộc, Quảng Nam",
+            "phone": "0327949281",
+            "age": 21
+        },
+    ]
--- api_beer/urls.py
+++ api_beer/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
 
 from api_beer.views import CreateProducerViewSet, BeerUnitViewSet, NationViewSet, BeerViewSet, BeerShipmentViewSet, \
-    BeerDetailViewSet, CartViewSet, BeerPhotoViewSet
+    BeerDetailViewSet, CartViewSet, BeerPhotoViewSet, OrderViewSet
+
 
 app_name = 'api_beer'
 router = routers.SimpleRouter(trailing_slash=True)
@@ -11,6 +12,7 @@ router.register(r'nation', NationViewSet, basename='nation')
 router.register(r'shipment', BeerShipmentViewSet, basename='beer_shipment')
 router.register(r'beerdetail', BeerDetailViewSet, basename='beer_user')
 router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'order', OrderViewSet, basename='order')
 router.register(r'photo', BeerPhotoViewSet, basename='beer_photo')
 router.register(r'', BeerViewSet, basename='beer')
 
