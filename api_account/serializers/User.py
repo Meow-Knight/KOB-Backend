efcb97d3ac7a4a4690756a8ae07a9ea35cbd9e57
bangpham <hoanglinhphan16@gmail.com>
2022-01-22 17:00:19 +0700

    [Fix] Order Checkout user and History order

--- api_account/serializers/User.py
+++ api_account/serializers/User.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from api_account.models import Account
-from api_account.serializers import AccountInfoSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -23,16 +22,6 @@ class ListUserSerializer(serializers.ModelSerializer):
         model = Account
         fields = ['first_name', 'last_name', 'address', 'phone', 'age', 'is_staff', ]
         depth = 1
-
-
-# class UserViewCheckoutSerializer(serializers.ModelSerializer):
-#     first_name = serializers.CharField(source='account.first_name')
-#     last_name = serializers.CharField(source='account.last_name')
-#     email = serializers.CharField(source='account.email')
-#
-#     class Meta:
-#         model = User
-#         fields = ['address', 'phone', 'age', 'first_name', 'last_name', 'email']
 
 
 class UserWithNameSerializer(serializers.ModelSerializer):
